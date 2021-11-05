4790e3c338a51daaf078cc8a4b1fe5ec71d7fa9f
CrazyDog1811 <evgeshatertyshnyi@mail.ru>
2021-11-05 21:58:47 +0300

    feat: add play-controller with js

---
+++ museum/style.js
@@ -0,0 +1,12 @@
+const progress = document.querySelector('.progress');
+const progress2 = document.querySelector('.volume-progr');
+
+progress.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`
+})
+
+progress2.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`
+})
